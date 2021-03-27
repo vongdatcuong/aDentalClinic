640de10a8e1c1cbf80da9dc589120fd3aea7cba8
The Anh <nhoxtheanh@gmail.com>
2021-03-27 16:39:30 +0700

    [Update - Patient Images]: sketch for Patient Images, using react-viewer to display images

--- src/components/feature/PatientImagesPage/index.js
+++ src/components/feature/PatientImagesPage/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-
+import Viewer from "react-viewer";
+import toothImg from "../../../assets/images/XRayTeeth.png";
 // @material-ui/core Component
 import Container from "@material-ui/core/Container";
 import styles from "./jss";
 import strings from "../../../configs/strings";
 import Typography from "@material-ui/core/Typography";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import { FaExpand } from "react-icons/fa";
 // use i18next
 import { useTranslation, Trans } from "react-i18next";
 import Button from "@material-ui/core/Button";
@@ -14,28 +21,134 @@ import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 // Component
 import PopupChat from "../../common/Messenger/PopupChat";
 
+const images = [
+  {
+    src:
+      "https://64.media.tumblr.com/4ce37f19e23ee5b7e1555f3cd1c0cac0/d51f00faf7b2b44f-76/s1280x1920/9a13a622e4a9da6600927f10fd3094a511d8fd76.jpg",
+    title: "Image 1",
+    date: "27/03/2021",
+  },
+  {
+    src:
+      "https://64.media.tumblr.com/5b10b8d5924e2e595ed390f413726ced/9bc8f6d4d09a2d4f-00/s1280x1920/ffffaccc6b9060e302daa73fc9b0d44b58597ab5.jpg",
+    title: "Image 2",
+    date: "27/03/2021",
+  },
+  {
+    src:
+      "https://64.media.tumblr.com/383c940ea2f2ba6aa41a6e593f26735b/5c8f2146b1afa07e-af/s1280x1920/3e721f1eb9d425f2ba88bc8329d54d8ba28e4ed2.jpg",
+    title: "Image 3",
+    date: "27/03/2021",
+  },
+  {
+    src:
+      "https://64.media.tumblr.com/e9cc31e93b7aad3829f8ad72be0d7c5b/5ead5ecdc6e8c4ff-77/s640x960/cb803ca5b21242e7eded54fb6d9d951f73c4bb04.jpg",
+    title: "Image 4",
+    date: "27/03/2021",
+  },
+  {
+    src:
+      "https://64.media.tumblr.com/d974c4699963d5a37678647daf11533a/199a8002a3510625-0f/s1280x1920/30f5be99dfcdd7206082435ebc60ef0139c26eef.jpg",
+    title: "Image 5",
+    date: "27/03/2021",
+  },
+  {
+    src: toothImg,
+    title: "Tooth 1",
+    date: "28/03/2021",
+  },
+  {
+    src: toothImg,
+    title: "Tooth 2",
+    date: "29/03/2021",
+  },
+  {
+    src: toothImg,
+    title: "Tooth 3",
+    date: "30/03/2021",
+  },
+  {
+    src: toothImg,
+    title: "Tooth 4",
+    date: "31/03/2021",
+  },
+  {
+    src: toothImg,
+    title: "Tooth 5",
+    date: "32/03/2021",
+  },
+];
+
 const useStyles = makeStyles(styles);
 
 const PatientImagesPage = () => {
-    const {t, i18n } = useTranslation();
-    const classes = useStyles();
+  const [visible, setVisible] = React.useState(false);
+  const [viewingImageIndex, setViewingImageIndex] = React.useState("0");
+  const { t, i18n } = useTranslation();
+  const classes = useStyles();
 
-    return (
-        <Container className={classes.container}>
-        <PopupChat></PopupChat>
-        <div className={classes.headerContainer}>
-          <Typography component="h1" variant="h5" className={classes.loginTitle}>
-            {t(strings.images)}
-          </Typography>
-          <Button simple className={classes.btnAddRecord}>
-            <AddCircleOutlineIcon></AddCircleOutlineIcon> {t(strings.addMoreImage)}
-          </Button>
-        </div>
-        <div className={classes.bodyContainer}>
+  function handleViewImage(clickedImageIndex) {
+    setViewingImageIndex(clickedImageIndex);
+    setVisible(true);
+  }
 
-        </div>
-      </Container>
-    )
-}
+  return (
+    <Container className={classes.container}>
+      <PopupChat></PopupChat>
+      <div className={classes.headerContainer}>
+        <Typography component="h1" variant="h5" className={classes.loginTitle}>
+          {t(strings.images)}
+        </Typography>
+        <Button simple className={classes.btnAddRecord}>
+          <AddCircleOutlineIcon></AddCircleOutlineIcon>{" "}
+          {t(strings.addMoreImage)}
+        </Button>
+      </div>
+      <div className={classes.bodyContainer}>
+        <GridList className={classes.gridList}>
+          {/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component="div">December</ListSubheader>
+        </GridListTile> */}
+          {images.map((image, index) => (
+            <GridListTile
+              key={image.img}
+              onClick={() => {
+                handleViewImage(index);
+              }}
+            >
+              <img
+                src={image.src}
+                alt={image.title}
+                className={classes.thumbnail}
+              />
+              <GridListTileBar
+                title={image.title}
+                subtitle={<span>date: {image.date}</span>}
+                actionIcon={
+                  <IconButton
+                    aria-label={`info about ${image.title}`}
+                    className={classes.icon}
+                  >
+                    <FaExpand />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+
+        <Viewer
+          visible={visible}
+          activeIndex={viewingImageIndex}
+          zoomSpeed="0.3"
+          onClose={() => {
+            setVisible(false);
+          }}
+          images={images}
+        />
+      </div>
+    </Container>
+  );
+};
 
 export default PatientImagesPage;
--- src/components/feature/PatientImagesPage/jss.js
+++ src/components/feature/PatientImagesPage/jss.js
@@ -10,12 +10,29 @@ const styles = (theme) => ({
     borderBottom: "solid " + theme.blackColor + " 0.05rem",
   },
   bodyContainer: {
-    padding: theme.spacing(10),
+    paddingTop: theme.spacing(10),
+    paddingLeft: theme.spacing(10),
+    paddingRight: theme.spacing(10),
+    "& .MuiGridList-root": {
+      "& .MuiGridListTile-root": {
+        width: "17vw!important",
+        height: "17vw!important",
+      },
+    },
   },
   btnAddRecord: {
     color: theme.primaryColor[0],
     fontWeight: "bold",
   },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
+  thumbnail: {
+    width: theme.spacing(40),
+    height: theme.spacing(40),
+    cursor: "pointer",
+  },
+
 });
 
 export default styles;
